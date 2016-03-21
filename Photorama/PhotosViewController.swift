ed47b89a3e208b591945935118c5b07cfa6e656a
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-03-21 06:58:35 +0100

    First Commit

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Rodrigo Villatoro on 3/21/16.
+//  Copyright © 2016 Rodrigo Villatoro. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
